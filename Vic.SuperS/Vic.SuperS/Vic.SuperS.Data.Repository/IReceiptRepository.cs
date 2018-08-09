ec7fd09c17ec6a82e27ee8abc90952600e85bfa4
AlexGao2018 <alex.gao2018@gmail.com>
2018-08-09 16:18:43 -0400

    Project SuperStore

---
+++ Vic.SuperS/Vic.SuperS/Vic.SuperS.Data.Repository/IReceiptRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Vic.SuperS.Data.Model;
+
+namespace Vic.SuperS.Data.Repository
+{
+    public interface  IReceiptRepository
+    {
+        Receipt  GetById(int id);
+        List<Receipt> GetAll();
+        //Receipt Add(Receipt receipt);
+        ////Receipt Update(int id, Product newValue);        
+    }
+}
---
+++ Vic.SuperS/Vic.SuperS/Vic.SuperS.Data.Repository/MockReceiptRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Vic.SuperS.Data.Model;
+
+
+namespace Vic.SuperS.Data.Repository
+{
+    public class MockReceiptRepository
+    {
+        public Receipt GetById(int id)
+        {
+            return 0;
+        }
+
+
+        List<Receipt> GetAll()
+        {
+
+            return 0;
+
+        }        
+    }
+}
