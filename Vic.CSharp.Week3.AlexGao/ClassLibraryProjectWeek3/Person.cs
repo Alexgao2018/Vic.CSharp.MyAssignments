72f0f3f988d57d655483f241d6649c15a15779e4
AlexGao2018 <alex.gao2018@gmail.com>
2018-08-09 16:13:32 -0400

    As3

---
+++ Vic.CSharp.Week3.AlexGao/ClassLibraryProjectWeek3/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryProjectWeek3
+{
+    class Person
+    {        
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Person_method_1()
+        {
+            Console.WriteLine("Person_method_1() is invoked");
+        }        
+    }
+}
---
+++ Vic.CSharp.Week3.AlexGao/ClassLibraryProjectWeek3/Teacher.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryProjectWeek3
+{
+    //class Teacher
+    //{
+    //}
+    class Teacher : Person
+    { 
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public int OfficeTel { get; set; }
+        public string[] Courses;
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine("++Teacher: Static Method Invoke---");
+        }
+
+        public void Teacher_Method_1()
+        {
+            Console.WriteLine("Teacher_method_1() is invoked");
+        }
+
+        
+        public bool UploadScores() { return true; }
+
+        public override string ToString()
+        {
+            return ("Teacher Name:{0} Faculty:{1} ----", Name, Faculty);
+        }
+    }
+}
